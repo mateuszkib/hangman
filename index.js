623874574029de117653e9bd23a5aa92daf38822
Mateusz <mateusz.kibilko@gmail.com>
2020-01-02 22:54:40 +0100

    Dynamic password from API

--- index.js
+++ index.js
@@ -4,19 +4,8 @@ hangmanImageContainer.style.background =
     '#cccccc url("/images/s0.gif") no-repeat right top';
 let errorContainer = document.querySelector(".error-container");
 let errorText = document.querySelector(".error-text");
-let categoryContainer = document.querySelector("#category-container");
-
-const passwords = [
-    { "Cristiano Ronaldo": "Footballer" },
-    { Mouse: "Accessories for PC" },
-    { Laptop: "Electronic equipment" },
-    { Tatry: "Polish mountain" },
-    { Iphone: "Electronic equipment" },
-    { "Joanna Brodzik": "Polish actress" },
-    { "Far Cry 5": "Game" },
-    { "Nie dolewaj oliwy do ognia": "Proverb" },
-    { "Kazdy kij ma dwa konce": "Proverb" }
-];
+const numberWords = 100;
+const apiKey = "FCH3RI55";
 
 class Hangman {
     constructor(word, guessedLetter = [], remainingGuesses = 8) {
@@ -25,6 +14,32 @@ class Hangman {
         this.remainingGuesses = remainingGuesses;
         this.counterGuess = 0;
         this.lettersToGuessed = "";
+        this.status = "playing";
+    }
+
+    setStatus() {
+        let finished = this.word.every(letter =>
+            this.guessedLetter.includes(letter)
+        );
+
+        if (finished) {
+            this.status = "finished";
+        } else if (this.remainingGuesses !== 0) {
+            this.status = "playing";
+        } else {
+            this.status = "lose";
+        }
+    }
+
+    get statusMessage() {
+        if (this.status === "finished") {
+            window.location.reload();
+            return `Great! You guess the password!`;
+        } else if (this.status === "playing") {
+            return `Guess left : ${this.remainingGuesses}`;
+        } else {
+            return `Ohh, you lose! The password was ${this.word.join("")}`;
+        }
     }
 
     drawLines() {
@@ -49,6 +64,8 @@ class Hangman {
         let includeLetter = this.guessedLetter.includes(letter);
         let isGoodGuess = this.word.includes(letter);
 
+        this.guessedLetter.push(letter);
+
         if (includeLetter) {
             errorText.innerHTML = "Już wykorzystałeś tę literę!";
             errorContainer.style.display = "block";
@@ -58,35 +75,33 @@ class Hangman {
             hangmanImageContainer.style.background = `url("/images/s${this.counterGuess}.gif") no-repeat right top`;
             this.remainingGuesses--;
         }
-
-        this.guessedLetter.push(letter);
         this.drawLines();
     }
 }
 
-function generateRandomPassword() {
-    const numberOfIndex = Math.floor(Math.random() * passwords.length) + 1;
-    let category = Object.values(passwords[numberOfIndex - 1]).join("");
-    let password = Object.keys(passwords[numberOfIndex - 1]).join("");
-    categoryContainer.textContent += category;
-    return {
-        category,
-        password
-    };
-}
+async function play() {
+    let words = await fetch(
+        `https://random-word-api.herokuapp.com/word?key=${apiKey}&number=${numberWords}`
+    )
+        .then(res => res.json())
+        .then(body => {
+            let wordsLength = body.length;
+            let index = Math.floor(Math.random() * wordsLength + 1);
+            const hangman1 = new Hangman(body[index]);
+            hangman1.drawLines();
+            const keyPress = function(e) {
+                hangman1.makeGuess(e.key);
+                hangman1.setStatus();
+                hangman1.statusMessage;
+                if (hangman1.remainingGuesses === 0) {
+                    window.removeEventListener("keydown", keyPress);
+                }
+            };
 
-function play() {
-    let generatedObject = generateRandomPassword();
-    const hangman1 = new Hangman(generatedObject.password);
-    hangman1.drawLines();
-    const keyPress = function(e) {
-        hangman1.makeGuess(e.key);
-        if (hangman1.remainingGuesses === 0) {
-            window.removeEventListener("keydown", keyPress);
-        }
-    };
-
-    window.addEventListener("keydown", keyPress);
+            window.addEventListener("keydown", keyPress);
+        })
+        .catch(err => console.log(err));
+    return words;
 }
 
 play();
